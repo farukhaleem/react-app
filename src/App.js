3f4096bd752359b17ed6e211075481baed63b458
farukhaleem <farrukhaleem31@gmail.com>
2021-02-03 19:16:47 +0500

    second change for auto deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        Deployement by Github Action
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Auto Deployment By Github Action</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
