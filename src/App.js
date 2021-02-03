f9a4595b4a1451ff31dabb9736a1e0d2a0d282ad
farukhaleem <farrukhaleem31@gmail.com>
2021-02-03 19:32:04 +0500

    auto deploy after adding surge token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Auto Deployment By Github Action</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      AutoDeploy With Surge Token Added in Repository
     </div>
   );
 }
